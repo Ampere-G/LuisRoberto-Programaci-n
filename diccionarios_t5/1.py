db29db90962a9f9dba7d1f5b19de03730e61a529
Ampere-G <157935905+Ampere-G@users.noreply.github.com>
2024-04-19 05:08:04 -0700

    1.py

--- diccionarios_t5/1.py
+++ diccionarios_t5/1.py
@@ -1,4 +1,4 @@
-#Escribe un programa en Python para multiplicar todos los elementos de un diccionario.
+#1 Escribe un programa en Python para multiplicar todos los elementos de un diccionario.
 
 numeros=dict(a=2,b=5,c=7,d=8)
 resultado=1
